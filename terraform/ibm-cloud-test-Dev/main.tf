d8594170c90d9485fedda0b89562336877a2f24f
herve <hgrange@fr.ibm.com>
2020-11-03 12:46:05 +0000

    201103

---
+++ terraform/ibm-cloud-test-Dev/main.tf
@@ -0,0 +1,38 @@
+#####################################################################
+##
+##      Created 10/12/20 by ucdpadmin for cloud ibmcloud. for test-cam-project2
+##
+#####################################################################
+
+## REFERENCE {"ibm_network":{"type": "ibm_reference_network"}}
+
+terraform {
+  required_version = "> 0.8.0"
+}
+
+provider "ibm" {
+  version = "~> 0.7"
+}
+
+resource "ibm_compute_vm_instance" "webserver" {
+  cores       = 1
+  memory      = 1024
+  domain      = "${var.vm_webserver_domain}"
+  hostname    = "${var.vm_webserver_hostname}"
+  datacenter  = "${var.vm_webserver_datacenter}"
+  ssh_key_ids = ["${ibm_compute_ssh_key.auth.id}"]
+  os_reference_code = "${var.vm_webserver_os_reference_code}"
+  hourly_billing = true
+  public_vlan_id       = "${var.ibm_network_public_vlan_id}"
+  private_vlan_id       = "${var.ibm_network_private_vlan_id}"
+}
+
+resource "tls_private_key" "ssh" {
+  algorithm = "RSA"
+}
+
+resource "ibm_compute_ssh_key" "auth" {
+  label = "CAM Public Key"
+  public_key = "${var.ibm_ssh_key_name}"
+}
+
