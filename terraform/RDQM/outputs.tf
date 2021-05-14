6d3e982e7d21fe55a5949a78b2890196c87bc718
hgrange <33756056+hgrange@users.noreply.github.com>
2021-05-14 10:26:49 +0200

    Update outputs.tf

--- terraform/RDQM/outputs.tf
+++ terraform/RDQM/outputs.tf
@@ -10,26 +10,17 @@ output "IP_address_server1" {
 output "IP_address2_server1" {
   value = "${ibm_compute_vm_instance.server1.ipv4_address_private}"
 }
-output "IP_address3_server1" {
-  value = "${ibm_compute_vm_instance.server1.secondary_ip_addresses.0}"
-}
 output "IP_address_server2" {
   value = "${ibm_compute_vm_instance.server2.ipv4_address}"
 }
 output "IP_address2_server2" {
   value = "${ibm_compute_vm_instance.server2.ipv4_address_private}"
 }
-output "IP_address3_server2" {
-  value = "${ibm_compute_vm_instance.server2.secondary_ip_addresses.0}"
-}
 output "IP_address_server3" {
   value = "${ibm_compute_vm_instance.server3.ipv4_address}"
 }
 output "IP_address2_server3" {
   value = "${ibm_compute_vm_instance.server3.ipv4_address_private}"
-}
-output "IP_address3_server3" {
-  value = "${ibm_compute_vm_instance.server3.secondary_ip_addresses.0}"
 }
 output "TestSecureField" {
   value = "${var.testsecurefield}"
